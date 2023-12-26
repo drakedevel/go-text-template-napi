a4f9e3df6af163b02ce31e9e488f7fc5e8ff2b3f
Andrew Drake <adrake@adrake.org>
2023-12-26 14:42:04 -0800

    Move NAPI_MODULE call to separate file for Node 18+ compatibility.

--- internal/napi/init.go
+++ internal/napi/init.go
@@ -1,10 +1,6 @@
 package napi
 
-// #cgo linux LDFLAGS: -Wl,--unresolved-symbols=ignore-all
-// #cgo darwin LDFLAGS: -Wl,-undefined,dynamic_lookup
 // #include <node_api.h>
-// napi_value napiModuleInit(napi_env env, napi_value exports);
-// NAPI_MODULE(go_text_template_napi_binding, napiModuleInit)
 import "C"
 
 var registeredInitFunc func(env Env, exports Value) (Value, error)
